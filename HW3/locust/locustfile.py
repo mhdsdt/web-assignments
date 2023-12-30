32a85b7d5420744251e318fbf2c312296504a9de
Mahdi <mahdi.saadatbakht.1381@gmail.com>
2023-12-30 16:32:14 +0330

    Add services in docker-compose.yml and add config for nginx

--- HW3/locust/locustfile.py
+++ HW3/locust/locustfile.py
@@ -1,3 +1,12 @@
 from locust import HttpUser, between, task
 
-# TODO PART 5
+class UserBehavior(HttpUser):
+    wait_time = between(1, 5)
+
+    @task(1)
+    def load_django(self):
+        self.client.get("http://django_backend")
+
+    @task(1)
+    def load_go(self):
+        self.client.get("http://go_backend")
